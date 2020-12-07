133c5fef61801e9e978d86453bea8133c4c0448c
linzi <873804682@qq.com>
2020-12-07 18:24:02 +0800

    fix

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -6,11 +6,12 @@
 
     <div class="right-menu">
       <template v-if="device!=='mobile'">
-        <search id="header-search" class="right-menu-item" />
+        <!-- <search id="header-search" class="right-menu-item" /> -->
 
-        <error-log class="errLog-container right-menu-item hover-effect" />
+        <!-- <error-log class="errLog-container right-menu-item hover-effect" /> -->
 
         <screenfull id="screenfull" class="right-menu-item hover-effect" />
+
 
         <el-tooltip content="Global Size" effect="dark" placement="bottom">
           <size-select id="size-select" class="right-menu-item hover-effect" />
@@ -20,22 +21,23 @@
 
       <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
         <div class="avatar-wrapper">
-          <img src="https://geekidea.oss-cn-chengdu.aliyuncs.com/spring-boot-plus/img/logo.png" class="user-avatar">
+          <!-- <img src="https://geekidea.oss-cn-chengdu.aliyuncs.com/spring-boot-plus/img/logo.png" class="user-avatar"> -->
+          <span>{{ userName }}</span>
           <i class="el-icon-caret-bottom" />
         </div>
         <el-dropdown-menu slot="dropdown">
-<!--          <router-link to="/profile/index">-->
-<!--            <el-dropdown-item>Profile</el-dropdown-item>-->
-<!--          </router-link>-->
-<!--          <router-link to="/">-->
-<!--            <el-dropdown-item>Dashboard</el-dropdown-item>-->
-<!--          </router-link>-->
-<!--          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">-->
-<!--            <el-dropdown-item>Github</el-dropdown-item>-->
-<!--          </a>-->
-<!--          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">-->
-<!--            <el-dropdown-item>Docs</el-dropdown-item>-->
-<!--          </a>-->
+          <!--          <router-link to="/profile/index">-->
+          <!--            <el-dropdown-item>Profile</el-dropdown-item>-->
+          <!--          </router-link>-->
+          <!--          <router-link to="/">-->
+          <!--            <el-dropdown-item>Dashboard</el-dropdown-item>-->
+          <!--          </router-link>-->
+          <!--          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">-->
+          <!--            <el-dropdown-item>Github</el-dropdown-item>-->
+          <!--          </a>-->
+          <!--          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">-->
+          <!--            <el-dropdown-item>Docs</el-dropdown-item>-->
+          <!--          </a>-->
           <el-dropdown-item divided @click.native="logout">
             <span style="display:block;">Log Out</span>
           </el-dropdown-item>
@@ -69,6 +71,11 @@ export default {
       'avatar',
       'device'
     ])
+  },
+  data() {
+    return {
+      userName: JSON.parse(sessionStorage.userInfo).loginSysUserVo.username
+    }
   },
   methods: {
     toggleSideBar() {
@@ -143,8 +150,10 @@ export default {
       margin-right: 30px;
 
       .avatar-wrapper {
-        margin-top: 5px;
+        // margin-top: 5px;
         position: relative;
+        display: flex;
+        align-items: center;
 
         .user-avatar {
           cursor: pointer;
